49dbb743f99afa2f63f1f76e472819d636835c9a
user <user@MacBook-Pro-Retina.local>
2020-02-27 22:26:52 +0400

    check

--- src/components/base/list.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const List = ({ items }) => (
-  <ul>
-    (items.map(item => <li>{item}</li>))
-  </ul>
-);
-export default List;
--- src/components/formAddToCart.js
+++ src/components/formAddToCart.js
@@ -1,27 +1,35 @@
 import React, { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addItem } from "../actions";
 
 const FormAddToCart = () => {
   const [goods, setGoods] = useState([]);
+  const [availebleGoods, setAvailebleGoods] = useState([]);
   const [chooseGood, setChooseGood] = useState();
+  
   const dispatch = useDispatch();
+  const cart = useSelector(state => state.cart);
 
   async function fetchGoods() {
     const data = await fetch("/data/goods.json").then(data => data.json());
-    
     setGoods(data);
   }
 
   useEffect(() => {
-   fetchGoods();
+    fetchGoods();
   }, []);
+
+  useEffect(() => {
+    setAvailebleGoods(
+      goods.filter(good => !cart.items.find(item => item.id === good.id))
+    );
+  }, [cart]);
 
   const handleChange = e => {
     setChooseGood(Number(e.target.value));
   };
 
-  const onAddGood = (e) => {
+  const onAddGood = e => {
     const addGood = goods.find(good => good.id === chooseGood);
 
     if (addGood) dispatch(addItem({ ...addGood, quantity: 1 }));
@@ -30,18 +38,25 @@ const FormAddToCart = () => {
 
   return (
     <form onSubmit={onAddGood}>
-      <label>
-        Choose in List
-        <select value={chooseGood} onChange={handleChange}>
-          {goods.map(({ id, name, price }) => (
-            <option key={name} value={id}>
-              {name} {price}$
-            </option>
-          ))}
-        </select>
-      </label>
+      {availebleGoods.length > 0 ? (
+        <>
+          <label>
+            Choose in List
+            <select value={chooseGood} onChange={handleChange} defaultValue="">
+              <option></option>
+              {availebleGoods.map(({ id, name, price }) => (
+                <option key={name} value={id}>
+                  {name} {price}$
+                </option>
+              ))}
+            </select>
+          </label>
 
-      <input type="submit" value="Add"/>
+          <input type="submit" value="Add" />
+        </>
+      ) : (
+        <p>No goods to add</p>
+      )}
     </form>
   );
 };
